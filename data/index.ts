38dc7f846d30a3aa4be84f8d62deb6557762cd05
parus <0Parus0@gmail.com>
2024-09-01 16:23:24 +0100

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a React Components library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "ParusUI",
+    des: "A digital market place where you can sell or buy your UI products",
+    img: "/parus-ui.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://parus-ui.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Parus-Eat",
+    des: "An enterprize level food ordering app where you can add, remove restaurants, menus, orders ",
+    img: "/parus-eat.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://parus-food-ordering-app.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Parus-Bookings",
+    des: "An enterprize level hotel booking app where you can book hotel rooms for adults and children",
+    img: "/hotel-booking.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://www.laca-sa.com/",
+  },
+  {
+    id: 4,
+    title: "Parus-Wild-Oasis",
+    des: "A hotel's internal management system dashboard, where you can manage hotel rooms and guests",
+    img: "/oasis.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://parus-wild-oasis.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Yasir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yasir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yasir is the ideal partner.",
+    name: "Ibrar Nasir",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Yasir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yasir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yasir is the ideal partner.",
+    name: "Jhangir Alam",
+    title: "DPT Director of Web Appics ",
+  },
+  {
+    quote:
+      "Collaborating with Yasir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yasir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yasir is the ideal partner.",
+    name: "Khurram Rashid",
+    title: "ASST Director of Web Appics ",
+  },
+  {
+    quote:
+      "Collaborating with Yasir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yasir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yasir is the ideal partner.",
+    name: "Sohail Ahmed",
+    title: "Full-Stack engineer of Web Appics ",
+  },
+  {
+    quote:
+      "Collaborating with Yasir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Yasir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Yasir is the ideal partner.",
+    name: "Ramiz Anwar",
+    title: "Back-End engineer of Web Appics ",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Junior Frontend Engineer",
+    desc: "Designed and developed web apps using React, Redux, Styled-components, TailwindCss.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Engineer",
+    desc: "Designed and developed web apps using React, React-Query, React-Router, TailwindCss.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Mid-Level Frontend Engineer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/0Parus0",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    href: "https://x.com/0Parus0",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/0parus0/",
+  },
+];
